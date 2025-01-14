f11d33efc27bce2d7dbcfef17317ebff48f6e4e7
Niyati <niyati@ucsb.edu>
2025-01-14 15:14:58 -0800

    finished hello world

---
+++ HelloWorldApp/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 32, // Adjust the value to make the text larger or smaller
+    fontWeight: 'bold', // Optional: make the text bold
+  },
+});
