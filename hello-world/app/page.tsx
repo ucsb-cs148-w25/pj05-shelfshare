3b49c55eea96876121b409a4a26f36a20a3a90e1
bella-ward <108088761+bella-ward@users.noreply.github.com>
2025-01-14 22:32:51 -0800

    bw - finished basic hello-world

---
+++ hello-world/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        margin: 0,
+      }}
+    >
+      <h1 style={{ fontSize: "48px" }}>Welcome to ShelfShare</h1>
+    </div>
+  );
+}
