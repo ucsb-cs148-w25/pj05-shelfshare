f07423963b220c6cc1e32b03a1309872de6cd1d2
Sungchae Park <spark686@ucsb.edu>
2025-01-15 03:20:30 -0800

    sp-created hello world web

---
+++ hello_world/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className= "font-bold text-8xl"> Hello World! </h1>
+    </div>
+  );
+}
