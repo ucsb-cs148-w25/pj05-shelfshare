b2e1794817e7590641fcbc2009bd926a1d9ad143
Towela Phiri <welaphiri@gmail.com>
2025-01-15 14:25:06 -0800

    created hello world application using react and next.js

---
+++ hello-world-towela/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Hello World :p",
+  description: "Brought to you by the hello world man",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="min-h-screen flex items-center justify-center" style={{ background: "linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
+
---
+++ hello-world-towela/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="text-6xl font-extrabold text-black drop-shadow-lg animate-pulse">
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
+
