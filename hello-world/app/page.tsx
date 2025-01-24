fa5b8b8a4d9a77000fd7631d9d3a134f7f841a39
Sungchae Park <spark686@ucsb.edu>
2025-01-23 18:11:21 -0800

    sp-copied and paste code for page.tsx from main

--- hello-world/app/page.tsx
+++ hello-world/app/page.tsx
@@ -1,22 +1,27 @@
-export default function Login() {
+// export default function Home() {
+//   return (
+//     <div className="flex justify-center items-center h-screen bg-gray-400">
+//       <div className="bg-pink-200 text-venter p-8 rounded-lg shadow-xl">
+//         <h1 className="text-4xl font-bold text-blue-700">Hello World</h1>
+//       </div>
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen background-login bg-no-repeat bg-center bg-contain">
-      <div className="mb-0 input_position_username">
-          {/* <label htmlFor="user_name" className="block mb-2 text-sm font-medium text-gray-900">Username</label> */}
-          <input type="text" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-800 focus:border-green-800 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="username"/>
-      </div>
-      <div className="mb-17 input_position_password">
-          {/* <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label> */}
-          <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-green-800 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-800 dark:focus:border-green-800" placeholder="•••••••••" required />   
-      </div>
-      <div>
-          <button className="mt-8 px-6 py-3 button-position opacity-75 bg-green-900 text-white text-2xl font-normal rounded-lg transform transition-all hover:scale-110">
-            LOG IN
-          </button>
+    <div className="flex justify-center items-center h-screen bg-green-200">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-green-800">Hello World</h1>
+        <button className="mt-8 px-6 py-3 bg-pink-400 text-white text-2xl font-normal rounded-full transform transition-all hover:scale-110 hover:bg-pink-500">
+          <span className="mr-2">💖</span>≽^•⩊•^≼
+        </button>
       </div>
     </div>
-
-
   );
 }
-
