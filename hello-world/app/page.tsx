6b2b62a62f77466bb6f51d9a3ccc7fa5e3c58999
bella-ward <108088761+bella-ward@users.noreply.github.com>
2025-01-23 22:28:39 -0800

    bw- saving changes

--- hello-world/app/page.tsx
+++ hello-world/app/page.tsx
@@ -67,21 +67,3 @@ export default function Browse() {
   );
 }
 
-
-
-
-
-
-// export default function Home() {
-//   return (
-//     <div className="flex justify-center items-center h-screen bg-green-200">
-//       <div className="text-center">
-//         <h1 className="text-4xl font-bold text-green-800">Hello! - World</h1>
-//         <button className="mt-8 px-6 py-3 bg-pink-400 text-white text-2xl font-normal rounded-full transform transition-all hover:scale-110 hover:bg-pink-500">
-//           <span className="mr-2">💖</span>≽^•⩊•^≼
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-
