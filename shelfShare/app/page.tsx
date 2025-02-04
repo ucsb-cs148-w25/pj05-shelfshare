667a926667a857946a444e1e8ac21391ccb08a65
charlene-patenaude <charlene_patenaude@ucsb.edu>
2025-02-03 20:22:59 -0800

    Removed deleted files

--- shelfShare/app/page.tsx
+++ shelfShare/app/page.tsx
@@ -1,16 +1,58 @@
-// app/page.tsx
-import './globals.css';
+'use client';
 
-export default function Home() {
+import { useAuth } from './context/AuthContext';
+import { useRouter } from 'next/navigation';
+
+export default function Login() {
+  const { signIn } = useAuth();
+  const router = useRouter();
+
+  const handleSignIn = async () => {
+    await signIn(); // Ensure signIn resolves before navigation
+    router.push('/home'); // Redirect to home page
+  };
+
   return (
-    <div className="flex justify-center items-center h-screen bg-custom-green">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold text-custom-tan">Hello ShelfShare</h1>
-        <button className="mt-8 px-6 py-3 bg-pink-400 text-white text-2xl font-normal rounded-full transform transition-all hover:scale-110 hover:bg-pink-500">
-          <span className="mr-2">💖</span>≽^•⩊•^≼
-        </button>
-      </div>
-    </div>
+    <div>
+     <img
+        src="/Books.png"
+        alt="books"
+        width="67%"
+        style={{top: "81%", position: "absolute"}}
+      />
+      <img
+        src="/Books.png"
+        alt="books2"
+        width="67%"
+        style={{top: "81%", position: "absolute", left: "65%"}}
+      />
+     <div style={{width: "100%", top: "24%", height: "40%",backgroundColor: "#847266", position: "absolute"}}> </div>
+     <div style={{width: "100%", top: "24%", height: "3%", backgroundColor: "#3D2F2A", position: "absolute"}}> </div>
+     <div style={{width: "100%", top: "62%", height: "3%", backgroundColor: "#3D2F2A", position: "absolute"}}> </div>
+     <img src="/login-logo.png" alt="book" width="40%" style={{top: "31%", left: "42%", width: "14%", position: "absolute"}}/>
+     {/* <div style={{
+       position: "absolute",
+       top: "36%",
+       left: "55%",
+       transform: "translate(-50%, -50%)",
+       color: "#DFDDCE",
+       fontSize: "3rem",
+       fontWeight: "bold"
+     }}>
+       ShelfShare
+     </div> */}
+     <button onClick={handleSignIn}
+     className="px-6 py-3 bg-custom-green text-white text-xl font-normal rounded-full transform transition-all hover:scale-110 hover:bg-blue-600"
+     style={{
+       position: "absolute", // Change to relative, fixed, or static as needed
+       top: "50%",           // Adjust vertical location
+       left: "43%",
+       width: "12%",
+             // Adjust horizontal location
+     }}
+   >
+       <span className="mr-1" ></span>Sign in with Google
+       </button>
+   </div>
   );
 }
-
--- shelfShare/app/profile/page.tsx
+++ shelfShare/app/profile/page.tsx
@@ -1,11 +1,59 @@
 // pages/profile.tsx
+'use client';
+
+import { useAuth } from '../context/AuthContext';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
 import React from 'react'
 
 const Profile = () => {
+    const { user } = useAuth();
+  const router = useRouter();
+
+  // Redirect to login page if user is not authenticated
+  useEffect(() => {
+    if (!user) {
+      router.push('/');
+    }
+  }, [user, router]);
+
+  if (!user) {
+    return null; // Avoid rendering anything while redirecting
+  }
+  
     return (
-        <div className="container mt-2">
-            This is a profile page
+    <div className="bg-[#5A7463] min-h-screen flex">
+            
+        <div className="flex flex-col items-center ml-48 mt-[39px]">
+        <h1 className="text-[#DFDDCE] text-4xl font-bold mb-10">
+            Profile
+        </h1>
+
+
+        {/* Profile Image */}
+        <img
+            src="/profile-pic.svg"
+            className="w-56 h-56 object-cover mb-4 rounded-full"
+            alt="Profile"
+        />
+
+
+        {/* Username Text */}
+        <p className="text-[#DFDDCE] text-xl">UserName</p>
         </div>
+        <div className= "flex flex-col justify-start pl-20 mr-20 items-start transform translate-x-14 mb-10">
+                <h2 className="text-[#DFDDCE] text-2xl font-bold mb-6">Preferred Genre:</h2>
+                <div className="bg-[#DFDDCE] text-[#3D2F2A] mt-2 p-4 rounded-lg text-lg mb-4 h-1/5">
+                #mystery #romance #fantasy
+                </div>
+                <h2 className="text-[#DFDDCE] text-2xl font-bold mb-6">About Me:</h2>
+                <div className="bg-[#DFDDCE] text-[#3D2F2A] mt-2 p-4 rounded-lg text-lg mb-4 h-1/5">
+                A blurb about the reader and whatever they want to put up.
+                </div>
+            </div>
+        </div>
+
     )
 }
 
