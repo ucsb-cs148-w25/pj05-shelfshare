fbfd0e754855fca41ce8e9e0982c13b615ae5f63
Niyati <niyati@ucsb.edu>
2025-02-09 17:52:51 -0800

    commented out one line for building

--- hello-world/app/books/page.tsx
+++ hello-world/app/books/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useAuth } from '../context/AuthContext';
-import { useRouter } from 'next/navigation';
+//import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import Image from "next/image";
 import {db} from "../../firebase"
