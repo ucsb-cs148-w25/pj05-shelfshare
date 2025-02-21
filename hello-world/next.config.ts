0f41f14b62aafa9ba029d95d9a150cc5fd6a3f66
Sungchae Park <spark686@ucsb.edu>
2025-02-21 15:04:56 -0800

    sp-fixed build issues

--- hello-world/next.config.ts
+++ hello-world/next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "covers.openlibrary.org",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
